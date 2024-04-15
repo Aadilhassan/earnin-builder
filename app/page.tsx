5e31429e8719b2289849f44946bdc07304858972
Aadil Hassan <aahanalhassan@gmail.com>
2024-04-15 09:10:28 +0530

    Add app/page.js to tsconfig.json include Update global CSS to change panel background color and button color Add dependencies for GrapesJS and other packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,175 @@
-import Image from "next/image";
+"use client"
+import { useEffect , useState} from "react";
+import grapesjs from 'grapesjs';
+import grapesjsPresetWebpage from 'grapesjs-preset-webpage';
+// import plugin from 'grapesjs-blocks-basic-extend'; 
+import plugin from 'grapesjs-blocks-basic';
+import NavBarPlugin from 'grapesjs-navbar';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import pluginTailwind from 'grapesjs-tailwind';
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+//npm i grapesjs-blocks-bootstrap4
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+import 'grapesjs/dist/css/grapes.min.css'
+import 'grapesjs/dist/grapes.min.js'
+import { readFile } from "fs";
+// import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.css'
+// import 'grapesjs-preset-webpage/dist/grapesjs-preset-webpage.min.js'
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+function WebBuilder() {
+
+
+
+  
+ useEffect(() => {
+
+
+  
+
+ // check if window is defined (so if in the browser or in node.js).
+  if (typeof window === 'undefined') {
+  } else {
+
+
+  Promise.all([
+    fetch('/index.html').then(response => response.text()),
+    fetch('/style.css').then(response => response.text())
+  ]).then(([html, css]) => {
+    const editor =  grapesjs.init({
+          container: '#gjs',
+          components: html,
+    
+          style: css,
+          height: '700px',
+          width: '100%',
+          panels: { defaults: [] },
+          
+          plugins: [ grapesjsPresetWebpage, pluginTailwind ],
+         //  pluginsOpts: {
+         //    [plugin]: { /* options */ }
+         //  },
+         canvas: {
+          styles: [
+            // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+          ],
+          scripts: [
+            'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+            // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js',
+            'https://cdn.skypack.dev/grapesjs-blocks-bootstrap5'
+          ],
+        },
+          storageManager: {
+            id: 'gjs-',
+            type: 'local',
+            autosave: true,
+            // storeComponents: true,
+            // storeStyles: true,
+            // storeHtml: true,
+            // storeCss: true,
+          },
+          deviceManager: {
+            devices:
+            [
+              {
+                id: 'desktop',
+                name: 'Desktop',
+                width: '',
+              },
+              {
+                id: 'tablet',
+                name: 'Tablet',
+                width: '768px',
+                widthMedia: '992px',
+              },
+              {
+                id: 'mobilePortrait',
+                name: 'Mobile portrait',
+                width: '320px',
+                widthMedia: '575px',
+              },
+            ]
+          },
+         //  pluginsOpts: {
+         //    'grapesjs-preset-webpage': {
+         //      blocksBasicOpts: {
+         //        blocks: ['column1', 'column2', 'column3', 'column3-7', 'text',     'link', 'image', 'video'],
+         //        flexGrid: 1,
+         //      },
+         //      blocks: ['link-block', 'quote', 'text-basic'],
+         //    },
+         //  }
+        })
+        editor.Panels.addPanel({
+          id: 'options',
+          el: '.panel__right',
+        });
+    
+     
+editor.Panels.addButton('options', {
+  id: 'download-html',
+  className: 'fa fa-download',
+  command: 'export-template',
+  attributes: {  },
+  label: 'Export HTML',
+tagName: 'span',
+  context: 'null',
+  buttons: [],
+  options: {},
+  active: false,
+  dragDrop: true,
+  togglable: true,
+  runDefaultCommand: true,
+  stopDefaultCommand: false,
+  disable: false,
+
+  // Add the other 6 properties here, set to null or an empty value
+});
+     
+editor.Commands.add('export-template', (editor, sender) => {
+  sender && sender.set('active', false);
+  const html = editor.getHtml();
+  const css = editor.getCss();
+  const zip = new JSZip();
+  zip.file('template.html', html);
+if (typeof css === 'string') {
+  zip.file('style.css', css);
 }
+
+  zip.generateAsync({ type: 'blob' })
+    .then(function(blob) {
+      saveAs(blob, 'template.zip');
+    });
+});
+      });
+
+
+    }
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+ },[])
+
+ return (
+   <div id="gjs">
+
+    
+   </div>
+ );
+}
+export default WebBuilder;
---
+++ declarations.d.ts
@@ -0,0 +1,4 @@
+declare module '*.js';
+declare module "@schukai/grapesjs-blocks-bootstrap5";
+declare module "grapesjs-blocks-bootstrap4";
+declare module "grapesjs-tailwind"
